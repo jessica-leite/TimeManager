a9d896066ed8a43d7c82815632521c18e00991dc
Jessica Leite <jessica.mleite@gmail.com>
2021-01-17 15:57:33 -0300

    Token request

--- FrontEnd/src/app/pages/login/login.component.ts
+++ FrontEnd/src/app/pages/login/login.component.ts
@@ -1,16 +1,22 @@
-import { Component, OnInit, OnDestroy } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit, OnDestroy {
-  constructor() {}
+export class LoginComponent implements OnInit {
+
+  constructor(private http: HttpClient) {}
 
   ngOnInit() {
   }
-  ngOnDestroy() {
+  
+  public getToken(email: string, password: string){
+    this.http.post('api/login', { email, password}, {
+      headers: new HttpHeaders({ 'Content-Type': 'application/json', 'responseType': 'text' })
+   })
+      .subscribe((data) => console.log("token response: " + data));
   }
-
 }
